8f76dae65670879c1d402bfc8a131d9dbdf1e43a
Graham Cockell <graham.cockell@outlook.com>
2016-03-12 14:21:24 +0000

    Initial commit

---
+++ AstroSimMain/Bootstrapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using Microsoft.Practices.Unity;
+
+using Prism.Unity;
+using Prism.Modularity;
+
+using ModuleA;
+
+namespace AstroSimMain
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            Type moduleAType = typeof(ModuleAModule);
+            ModuleCatalog.AddModule(new ModuleInfo()
+                {
+                    ModuleName = moduleAType.Name,
+                    ModuleType = moduleAType.AssemblyQualifiedName,
+                    InitializationMode = InitializationMode.WhenAvailable
+                }
+            );
+        }
+    }
+}
---
+++ Modules/ModuleA/ModuleAModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Practices.Unity;
+
+using Prism.Modularity;
+using Prism.Unity;
+using Prism.Regions;
+
+namespace ModuleA
+{
+    public class ModuleAModule : IModule
+    {
+        private IUnityContainer container;
+        private IRegionManager regionManager;
+
+        public ModuleAModule( IUnityContainer container, IRegionManager regionManager )
+        {
+            this.container = container;
+            this.regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            regionManager.RegisterViewWithRegion("ToolbarRegion", typeof(ToolbarView));
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(ContentView));
+        }
+    }
+}
